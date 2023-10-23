544a6808595755ebedd9e7aca65e68c9751eaa47
Vishnu S Nair <vsnair007@gmail.com>
2023-10-23 14:40:17 +0530

    Project created

---
+++ src/test/java/com/mundackal/EcommService/EcommServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mundackal.EcommService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
